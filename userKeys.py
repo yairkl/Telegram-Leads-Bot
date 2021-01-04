58f7a84fa83dfbb137db3e9319facbdf40f53dd1
yairkl <yair@yedidia.net>
2021-01-04 15:26:35 +0200

    Create userKeys.py

---
+++ userKeys.py
@@ -0,0 +1,6 @@
+api_id = 0#telegram api id
+api_hash = ''#telegram api hash
+links_group = 0#links group id
+admins = [] # admins id list
+proxy=None# proxy configuration in the following format ("http", '127.0.0.1', 8080)
+bot_token = ''#telegram bot token
