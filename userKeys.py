aa783feb942e36854d782fcd25ff9e3d7c42945f
yairkl <yair@yedidia.net>
2021-10-28 21:15:26 +0300

    Update userKeys.py

--- userKeys.py
+++ userKeys.py
@@ -1,7 +1,7 @@
-api_id = 2984129
-api_hash = '2de604453ac93a5d515e087786df12fb'
-links_group = -472251819
-admins = [-472251819,88439707]
-proxy=("http", '192.168.100.127', 8000)
-bot_token = '1451837111:AAE2R4cUiylX1n9CNyZIDXcbWO_XwHTsPno'
+api_id = 0
+api_hash = ''
+links_group = -1
+admins = []
+proxy=("http", 'host', 'port') #optional
+bot_token = ''
 info_user='@someUser'
